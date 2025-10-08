5bbe535eaeb49a75018f6ff637ce75733865734b
Yu Duo Zhang <2480549@edu.vaniercollege.qc.ca>
2025-10-08 11:33:58 -0400

    Initialize Task 02 File

---
+++ src/vaniercollege/zyd/YuDuoZ_Lab05_Task02.java
@@ -0,0 +1,23 @@
+package vaniercollege.zyd;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Yu Duo Zhang (2480549)
+ * https://github.com/zyd-074/Vanier_F25_420-SF3-RE_Lab05.git
+ */
+public class YuDuoZ_Lab05_Task02 extends Application {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    
+    @Override
+    public void start(Stage stage) {
+        
+    }
+}
