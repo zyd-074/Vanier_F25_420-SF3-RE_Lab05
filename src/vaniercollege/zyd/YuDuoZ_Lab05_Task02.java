85de3390f6e2f708e66fa73597e6611389defd31
Yu Duo Zhang <2480549@edu.vaniercollege.qc.ca>
2025-10-08 12:01:20 -0400

    feat: Add Menu List

--- src/vaniercollege/zyd/YuDuoZ_Lab05_Task02.java
+++ src/vaniercollege/zyd/YuDuoZ_Lab05_Task02.java
@@ -1,6 +1,9 @@
 package vaniercollege.zyd;
 
+import java.util.ArrayList;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
@@ -28,9 +31,53 @@ public class YuDuoZ_Lab05_Task02 extends Application {
         category.getItems().addAll("Beverage", "Appetizer", "Main Course", "Dessert");
         selectControl.getChildren().addAll(categorySelect, category);
         
-        root.setLeft(selectControl);
+        ArrayList<String> beverageList = new ArrayList<>();
+        ArrayList<String> appetizerList = new ArrayList<>();
+        ArrayList<String> mainList = new ArrayList<>();
+        ArrayList<String> dessertList = new ArrayList<>();
+        {beverageList.add("Coffee | 2.50");
+        beverageList.add("Tea | 2.00");
+        beverageList.add("Soft Drink | 1.75");
+        beverageList.add("Water | 2.95");
+        beverageList.add("Milk | 1.5");
+        beverageList.add("Juice | 2.5");
+        appetizerList.add("Soup | 4.50");
+        appetizerList.add("Salad | 3.75");
+        appetizerList.add("Spring Rolls | 5.25");
+        appetizerList.add("Garlic Bread | 3.00");
+        appetizerList.add("Chips and Salsa | 6.95");
+        mainList.add("Steak | 15.00");
+        mainList.add("Grilled Chicken | 13.50");
+        mainList.add("Chicken Alfredo | 13.95");
+        mainList.add("Turkey Club | 11.90");
+        mainList.add("Shrimp Scampi | 18.99");
+        mainList.add("Pasta | 11.75");
+        mainList.add("Fish and Chips | 12.25");
+        dessertList.add("Apple Pie | 5.95");
+        dessertList.add("Carrot Cake | 4.50");
+        dessertList.add("Mud Pie | 4.75");
+        dessertList.add("Pudding | 3.25");
+        dessertList.add("Apple Crisp | 5.98");} // Add Menu
+        ObservableList<String> beverage = FXCollections.observableArrayList(beverageList);
+        ObservableList<String> appetizer = FXCollections.observableArrayList(appetizerList);
+        ObservableList<String> mainCourse = FXCollections.observableArrayList(mainList);
+        ObservableList<String> dessert = FXCollections.observableArrayList(dessertList);
+        ListView<String> beverageMenu = new ListView<>(beverage);
+        ListView<String> appetizerMenu = new ListView<>(appetizer);
+        ListView<String> mainMenu = new ListView<>(mainCourse);
+        ListView<String> dessertMenu = new ListView<>(dessert);
         
-        Scene scene = new Scene(root);
+        root.setLeft(selectControl);
+        category.setOnAction(e -> {
+            switch (category.getValue().toString()) {
+                case "Beverage" -> root.setCenter(beverageMenu);
+                case "Appetizer" -> root.setCenter(appetizerMenu);
+                case "Main Course" -> root.setCenter(mainMenu);
+                case "Dessert" -> root.setCenter(dessertMenu);
+            }
+        });
+        
+        Scene scene = new Scene(root, 500, 200);
         stage.setScene(scene);
         stage.show();
     }
